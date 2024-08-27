bebf2c51f7db5c61b1132bbe40f2eb88089b8b39
Jason Lee <jason@zerodevx.com>
2024-08-28 00:38:50 +0800

    Add basic tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,16 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
-  await page.goto('/')
-  expect(await page.textContent('h1')).toBe('Welcome to SvelteKit')
+test('basic prettify', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+  const textarea = page.locator('textarea')
+  await textarea.click()
+  await textarea.pressSequentially(` {foo:'bar'}`)
+  await page.getByText('MAKE IT PRETTY').click()
+  await page.screenshot({ path: 'test-results/screenshot.png' })
+  expect(await page.locator('.wrap').textContent()).toBe(`{ "foo": "bar"  }`)
+})
+
+test('basic decode', async ({ page }) => {
+  await page.goto('/H4sIAAAAAAAAA1OoTsvPt1JPSixSrwUAdjjZPQwAAAA/', { waitUntil: 'networkidle' })
+  expect(await page.locator('.wrap').textContent()).toBe(`{ "foo": "bar"  }`)
 })
