3482e6318a5807c9562f962f45d04558ba0f6e70
Jason Lee <jason@zerodevx.com>
2026-08-03 15:51:13 +0800

    build: setup src structure

--- src/routes/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-import { page } from '$app/stores'
-</script>
-
-<div class="prose mx-auto mt-8 text-center">
-  <h1>ERR {$page.status}</h1>
-  <p>Whoops! Server says: "{$page.error?.message}"</p>
-</div>
