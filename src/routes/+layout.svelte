afaf7a74cea05a9550fe694e174b2f6531ddb577
Jason Lee <jason@zerodevx.com>
2026-08-05 04:14:28 +0800

    revert: logo back into `assets/`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
 import './layout.css'
+import logo from '$lib/assets/logo.svg'
 
 let { children } = $props()
 </script>
+
+<svelte:head>
+  <link rel="icon" href={logo} />
+  <title>JSON Pretty Print | Open-source, no tracking, no ads</title>
+  <meta
+    name="description"
+    content="Prettify JSON on-device, then send the tree view as a shareable web link. Open-source, no tracking, no ads."
+  />
+</svelte:head>
 
 {@render children()}
