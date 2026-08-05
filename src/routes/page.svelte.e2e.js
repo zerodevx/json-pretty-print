d4ca4c4bcbc3c629f85a899c731fecd300452def
Jason Lee <jason@zerodevx.com>
2026-08-06 01:54:11 +0800

    test: add basic tests

--- src/routes/page.svelte.e2e.js
+++ src/routes/page.svelte.e2e.js
@@ -1,6 +1,30 @@
 import { expect, test } from '@playwright/test'
 
-test('has expected h1', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.locator('h1')).toBeVisible()
+const b = (url = '') => `/json-pretty-print${url}`
+
+test.describe('Basic', () => {
+  test('prettify forgivingly', async ({ page }) => {
+    await page.goto(b('/'))
+    await page.locator('textarea').click()
+    await page.locator('textarea').pressSequentially(` {foo:'bar'}`)
+    await page.getByRole('button', { name: 'MAKE IT PRETTY' }).click()
+    expect(await page.locator('.view').textContent()).toBe(`{ "foo": "bar" }`)
+    await page.getByRole('button', { name: 'Close' }).click()
+    await expect(page.locator('textarea')).toHaveValue(` {foo:'bar'}`)
+  })
+
+  test('hash link view', async ({ page }) => {
+    await page.goto(b('/#/H4sIAAAAAAAAE1OoTsvPt1JPSixSrwUAdjjZPQwAAAA'))
+    expect(await page.locator('.view').textContent()).toBe(`{ "foo": "bar" }`)
+    await page.getByRole('button', { name: 'Close' }).click()
+    await expect(page.locator('textarea')).toHaveValue(` {foo:'bar'}`)
+  })
+})
+
+test.describe('Backward compatibility', () => {
+  test('redirect legacy web link', async ({ page }) => {
+    await page.goto(b('/H4sIAAAAAAAAE1OoTsvPt1JPSixSrwUAdjjZPQwAAAA/'))
+    expect(await page.locator('.view').textContent()).toBe(`{ "foo": "bar" }`)
+    await expect(page).toHaveURL(/.*\/#\/H4sIAAAAAAAAE1OoTsvPt1JPSixSrwUAdjjZPQwAAAA$/)
+  })
 })
